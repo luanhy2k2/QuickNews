0f978f2fc870a05bb5e2923d3b4c1b2781e73b68
luanhy2k2 <luan2k2hy@gmail.com>
2024-09-13 23:42:59 +0700

    update genericService

--- app/Services/Contracts/IGenericService.php
+++ app/Services/Contracts/IGenericService.php
@@ -1,7 +1,7 @@
 <?php
 namespace App\Services\Contracts;
 interface IGenericService{
-    // public function get(int $pageIndex, int $pageSize, string $keyword);
+    public function get(int $pageIndex, int $pageSize, string $keyword);
     public function find($id);
     public function create($data);
     public function update($id, $data);
